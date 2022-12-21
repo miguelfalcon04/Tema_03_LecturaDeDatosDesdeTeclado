a7eb0aefaadf2831191170d47d573d438c2f5473
miguelfal <falconmiguel.2004@gmail.com>
2022-12-21 21:45:07 +0100

    Commit

---
+++ Eje02.java
@@ -0,0 +1,20 @@
+/* Ejercicio 2
+Realiza un conversor de euros a pesetas. La cantidad de euros que se quiere
+convertir debe ser introducida por teclado.
+
+@author miguelfalcon
+*/
+
+import java.util.Scanner;
+public class Eje02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Euros que va a pasar a pesetas: ");
+        int euros = sc.nextInt();
+
+        System.out.println(euros+" euros son "+euros*166+" en pesetas");
+
+        sc.close();
+    }
+}
