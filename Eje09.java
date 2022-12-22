115d2b594342da2a4b03be95c8813830f9650e5d
miguelfal <falconmiguel.2004@gmail.com>
2022-12-22 16:49:16 +0100

    commit

---
+++ Eje09.java
@@ -0,0 +1,23 @@
+/* Ejercicio 9
+Escribe un programa que calcule el volumen de un cono según la fórmula V =1/3πr^2h
+
+@author miguelfalcon
+*/
+
+import java.util.Scanner;;
+public class Eje09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduzca el radio: ");
+        double r = sc.nextInt();
+
+        System.out.print("Introduzca la altura: ");
+        double h = sc.nextInt();
+
+        System.out.printf("El volumen del cono será de: %.2f",((1.0/3.0)*Math.PI*r*r*h));
+
+        sc.close();
+
+    }
+}
